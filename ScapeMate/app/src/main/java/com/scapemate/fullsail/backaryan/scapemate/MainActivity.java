2b4f02aaf484c0cf1416f61113831728a8936c98
RyanBacka <RKBacka@Fullsail.edu>
2017-05-02 22:14:19 -0400

    First Submission
    Project creation

---
+++ ScapeMate/app/src/main/java/com/scapemate/fullsail/backaryan/scapemate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.scapemate.fullsail.backaryan.scapemate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
