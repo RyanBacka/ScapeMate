e9ada3b653ea683082f2f87f359f6928176da8b7
RyanBacka <RKBacka@Fullsail.edu>
2017-05-25 17:02:54 -0400

    Finished Project

--- ScapeMate/app/src/main/java/com/scapemate/fullsail/backaryan/scapemate/fragments/SelectedBidFragment.java
+++ ScapeMate/app/src/main/java/com/scapemate/fullsail/backaryan/scapemate/fragments/SelectedBidFragment.java
@@ -1,12 +1,15 @@
 package com.scapemate.fullsail.backaryan.scapemate.fragments;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -29,9 +32,10 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class SelectedBidFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener{
+public class SelectedBidFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "SelectedBidFrag";
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0x00100;
     DataHelper dataHelper = new DataHelper();
     Company company;
     Bid bid;
@@ -46,10 +50,10 @@ public class SelectedBidFragment extends Fragment implements View.OnClickListene
         setHasOptionsMenu(true);
     }
 
-    public static SelectedBidFragment newInstance(int selected){
+    public static SelectedBidFragment newInstance(int selected) {
         SelectedBidFragment fragment = new SelectedBidFragment();
         Bundle args = new Bundle();
-        args.putInt("selected",selected);
+        args.putInt("selected", selected);
         fragment.setArguments(args);
         return fragment;
     }
@@ -64,20 +68,20 @@ public class SelectedBidFragment extends Fragment implements View.OnClickListene
         ArrayList<Bid> bids = company.getBids();
         int selected = getArguments().getInt("selected");
         bid = bids.get(selected);
-        ((TextView)view.findViewById(R.id.selectedCustomerName)).setText(bid.getCustomerName());
-        ((TextView)view.findViewById(R.id.selectedCustomerAddress)).setText(bid.getCustomerAddress());
-        ((TextView)view.findViewById(R.id.selectedCustomerPhone)).setText(bid.getCustomerPhoneNum());
+        ((TextView) view.findViewById(R.id.selectedCustomerName)).setText(bid.getCustomerName());
+        ((TextView) view.findViewById(R.id.selectedCustomerAddress)).setText(bid.getCustomerAddress());
+        ((TextView) view.findViewById(R.id.selectedCustomerPhone)).setText(bid.getCustomerPhoneNum());
         BidHelper bidHelper = new BidHelper();
-        String manRate = "Man Rate: "+ String.format(Locale.ENGLISH,"%.2f", bidHelper.getManRate(company,bid));
-        ((TextView)view.findViewById(R.id.selectedManRate)).setText(manRate);
-        String materialCost = "Material Cost: "+String.format(Locale.ENGLISH,"%.2f", bidHelper.getMaterialCost(bid));
-        ((TextView)view.findViewById(R.id.selectedMaterialCost)).setText(materialCost);
-        String breakEven = "Break Even: "+String.format(Locale.ENGLISH, "%.2f", bidHelper.getBreakEven(company,bid));
-        ((TextView)view.findViewById(R.id.selectedBreakEven)).setText(breakEven);
-        String bidPrice = "Bid Price: "+String.format(Locale.ENGLISH, "%.2f", bidHelper.getEstimatePrice(company,bid));
-        ((TextView)view.findViewById(R.id.selectedBidPrice)).setText(bidPrice);
-        ((Spinner)view.findViewById(R.id.statusSpinner)).setOnItemSelectedListener(this);
-        ((Spinner)view.findViewById(R.id.statusSpinner)).setSelection(bid.getBidStatus());
+        String manRate = "Man Rate: " + String.format(Locale.ENGLISH, "%.2f", bidHelper.getManRate(company, bid));
+        ((TextView) view.findViewById(R.id.selectedManRate)).setText(manRate);
+        String materialCost = "Material Cost: " + String.format(Locale.ENGLISH, "%.2f", bidHelper.getMaterialCost(bid));
+        ((TextView) view.findViewById(R.id.selectedMaterialCost)).setText(materialCost);
+        String breakEven = "Break Even: " + String.format(Locale.ENGLISH, "%.2f", bidHelper.getBreakEven(company, bid));
+        ((TextView) view.findViewById(R.id.selectedBreakEven)).setText(breakEven);
+        String bidPrice = "Bid Price: " + String.format(Locale.ENGLISH, "%.2f", bidHelper.getEstimatePrice(company, bid));
+        ((TextView) view.findViewById(R.id.selectedBidPrice)).setText(bidPrice);
+        ((Spinner) view.findViewById(R.id.statusSpinner)).setOnItemSelectedListener(this);
+        ((Spinner) view.findViewById(R.id.statusSpinner)).setSelection(bid.getBidStatus());
         (view.findViewById(R.id.scheduleButton)).setOnClickListener(this);
         (view.findViewById(R.id.selectedBidSave)).setOnClickListener(this);
         return view;
@@ -92,26 +96,34 @@ public class SelectedBidFragment extends Fragment implements View.OnClickListene
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.selectedBidSave){
+        if (v.getId() == R.id.selectedBidSave) {
             BidListFragment bidListFragment = BidListFragment.newInstance();
             getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.listContainer,bidListFragment)
+                    .replace(R.id.listContainer, bidListFragment)
                     .commit();
 
-        } else if(v.getId()==R.id.scheduleButton){
-            Calendar calendar = Calendar.getInstance();
-            double jobLength = bid.getJobHours();
+        } else if (v.getId() == R.id.scheduleButton) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CALENDAR, Manifest.permission.WRITE_CALENDAR}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            } else if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_CALENDAR) == PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_CALENDAR) == PackageManager.PERMISSION_GRANTED) {
 
-            Intent intent = new Intent(Intent.ACTION_INSERT)
-                    .setData(CalendarContract.Events.CONTENT_URI)
-                    .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calendar.getTimeInMillis());
-                    calendar.add(Calendar.HOUR,+(int)jobLength);
-                    intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, calendar.getTimeInMillis())
-                    .putExtra(CalendarContract.Events.TITLE, bid.getCustomerName())
-                    .putExtra(CalendarContract.Events.DESCRIPTION, bid.getJobType())
-                    .putExtra(CalendarContract.Events.EVENT_LOCATION, bid.getCustomerAddress())
-                    .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
-            startActivity(intent);
+
+                Calendar calendar = Calendar.getInstance();
+                double jobLength = bid.getJobHours();
+
+                Intent intent = new Intent(Intent.ACTION_INSERT)
+                        .setData(CalendarContract.Events.CONTENT_URI)
+                        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, calendar.getTimeInMillis());
+                calendar.add(Calendar.HOUR, +(int) jobLength);
+                intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, calendar.getTimeInMillis())
+                        .putExtra(CalendarContract.Events.TITLE, bid.getCustomerName())
+                        .putExtra(CalendarContract.Events.DESCRIPTION, bid.getJobType())
+                        .putExtra(CalendarContract.Events.EVENT_LOCATION, bid.getCustomerAddress())
+                        .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
+                startActivity(intent);
+            }
         }
     }
 
@@ -120,21 +132,21 @@ public class SelectedBidFragment extends Fragment implements View.OnClickListene
         String bidStatus = ((Spinner) getActivity().findViewById(R.id.statusSpinner)).getSelectedItem().toString();
         int selected = getArguments().getInt("selected");
         ArrayList<Bid> bids = company.getBids();
-        if(bidStatus.equalsIgnoreCase("accepted")){
+        if (bidStatus.equalsIgnoreCase("accepted")) {
             bid.updateBidStatus(1);
-            bids.set(selected,bid);
+            bids.set(selected, bid);
             company.addBid(bids);
-            dataHelper.saveCompany(company,getActivity());
-        }else if(bidStatus.equalsIgnoreCase("pending")){
+            dataHelper.saveCompany(company, getActivity());
+        } else if (bidStatus.equalsIgnoreCase("pending")) {
             bid.updateBidStatus(0);
-            bids.set(selected,bid);
+            bids.set(selected, bid);
             company.addBid(bids);
-            dataHelper.saveCompany(company,getActivity());
-        }else if(bidStatus.equalsIgnoreCase("denied")){
+            dataHelper.saveCompany(company, getActivity());
+        } else if (bidStatus.equalsIgnoreCase("denied")) {
             bid.updateBidStatus(2);
-            bids.set(selected,bid);
+            bids.set(selected, bid);
             company.addBid(bids);
-            dataHelper.saveCompany(company,getActivity());
+            dataHelper.saveCompany(company, getActivity());
         }
     }
 
